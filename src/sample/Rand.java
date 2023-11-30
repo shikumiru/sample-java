e288cccaa7ffd1728df429927cf811c94b71ba23
shikumiru <>
2023-11-30 20:22:24 +0900

    random

---
+++ src/sample/Rand.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Rand {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int r = new java.util.Random().nextInt(90);
+		System.out.println("あなたは" + r + "歳ですか？");
+
+	}
+
+}
