312b0fcadb2d593a8515411a563081e818bcf989
shikumiru <>
2023-11-30 20:18:00 +0900

    文字列を整数に変換

---
+++ src/sample/ParseInt.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class ParseInt {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String age = "31";
+		//	文字列を整数に変換する	
+		int n = Integer.parseInt(age);
+		System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+
+	}
+
+}
