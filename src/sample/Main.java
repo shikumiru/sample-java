cb753849907496814841c4d4b63b5c9b5a34b3fa
shikumiru <>
2023-11-30 19:40:10 +0900

    Math.max

---
+++ src/sample/Main.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 5;
+		int b = 3;
+        //	a と b　で大きい数値を代入するメソッド	
+		int m = Math.max(a, b);
+		System.out.println("比較実験：" + a + "と" + b + "で大きいほうは…" + m );
+
+	}
+
+}
