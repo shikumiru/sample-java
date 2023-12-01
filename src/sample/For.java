eb59556ad22ad6f029c0b0197fed1caa0fe9bdd6
shikumiru <>
2023-12-01 23:18:45 +0900

    kuku

--- src/sample/For.java
+++ src/sample/For.java
@@ -4,8 +4,8 @@ public class For {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		for (int i =0; i < 10; i++) {
-			System.out.println("こんにちは");
+		for (int i =0; i < 3; i++) {
+			System.out.println("現在" + (i +1) + "周目→");
 		}
 
 	}
