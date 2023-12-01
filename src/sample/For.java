cbeb32b777fbb4ec579453d1058c8feec59eb44a
shikumiru <>
2023-12-01 23:07:30 +0900

    For文

---
+++ src/sample/For.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class For {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i =0; i < 10; i++) {
+			System.out.println("こんにちは");
+		}
+
+	}
+
+}
