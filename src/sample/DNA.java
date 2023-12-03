c50204a06b6535d91d0bee7a1770c31db99dd424
shikumiru <>
2023-12-03 21:21:20 +0900

    拡張for文

---
+++ src/sample/DNA.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class DNA {
+
+	public static void main(String[] args) {
+		int[] seq = new int[10];
+		
+		for (int i = 0; i < seq.length; i++) {
+			seq[i] = new java.util.Random().nextInt(4);
+		}
+		
+		for (int i = 0; i < seq.length; i++) {
+			char[] base = {'A', 'T', 'G', 'C'};
+			System.out.print(base[seq[i]] + " ");
+		}
+	}
+
+}
---
+++ src/sample/For2.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		int[] scores = {20, 30, 40, 50, 80};
+		for (int value : scores) {
+			System.out.println(value);
+		}
+	}
+
+}
