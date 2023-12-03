945cae1f545108b70bdbe221121b4d59b1edef50
shikumiru <>
2023-12-03 21:53:40 +0900

    fix

--- src/sample/Score.java
+++ src/sample/Score.java
@@ -3,7 +3,7 @@ package sample;
 public class Score {
 
 	public static void main(String[] args) {
-		int[] scores = {20, 30, 40, 50, 80};
+		int[] scores = {22, 56, 40, 50, 80};
 		int sum = 0;
 		for (int i = 0; i < scores.length; i++) {
 			System.out.println(scores[i]);
