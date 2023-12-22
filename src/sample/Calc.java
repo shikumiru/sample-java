fd57bd64c37c8c9c2ae801bef8a4b6ac90bbfaf4
shikumiru <code.0signal@gmail.com>
2023-12-22 16:51:45 +0900

    計算

--- src/sample/Calc.java
+++ src/sample/Calc.java
@@ -1,17 +1,14 @@
 package sample;
 
+import java.util.Random;
+
 public class Calc {
 	public static void main(String[] args) {
-		int a = 10; int b = 2;
-		int total = tasu(a, b);
-		int delta = hiku(a, b);
-		System.out.println("足すと" + total + "、引くと" + delta);
-	}
-	public static int tasu(int a, int b) {
-		return (a + b);
-	}
-	public static int hiku(int a, int b) {
-		return (a - b);
+		Random rand = new Random();
+		int a = rand.nextInt(10); int b = rand.nextInt(10);
+		int total = CalcLogic.tasu(a, b);
+		int delta = CalcLogic.hiku(a, b);
+		System.out.println(a + "と" + b + "足すと" + total + "、引くと" + delta);
 	}
 
 }
