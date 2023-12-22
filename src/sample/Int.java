aa6e115d79fa888124b66cc48936dd2c56f4c4e9
shikumiru <code.0signal@gmail.com>
2023-12-22 13:01:37 +0900

    メソッド

---
+++ src/sample/Int.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Int {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		add(100, 20);
+		add(200, 50);
+
+	}
+	public static void add(int x, int y) {
+		int ans = x + y;
+		System.out.println(x + "+" + y + "=" + ans);
+	}
+
+}
