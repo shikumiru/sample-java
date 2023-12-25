fe62dabc810e243b66ec28c0087c4fde8087c0f5
shikumiru <code.0signal@gmail.com>
2023-12-25 17:06:00 +0900

    hero

---
+++ src/hero/Hero.java
@@ -0,0 +1,27 @@
+package hero;
+
+public class Hero {
+	
+	String name;
+	int hp;
+	public void sleep() {
+		this.hp = 100;
+		System.out.println(this.name + "は、眠って回復");
+	}
+	public void sit(int sec) {
+		this.hp += sec;
+		System.out.println(this.name + "は、" + sec + "秒座った");
+		System.out.println("HPが" + sec + "ポイント回復");
+	}
+	public void slip() {
+		this.hp -= 5;
+		System.out.println(this.name + "は、転んだ");
+		System.out.println("5ダメージ");
+	}
+	public void run() {
+		System.out.println(this.name + "は、逃げ出した");
+		System.out.println("GAMEOVER");
+		System.out.println("最終HP" + this.hp);
+	}
+
+}
